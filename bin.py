3f662cdf93fa8552e364f5c245bde7d35d5639e5
unknown <kishorekumarsrini2006@gmail.com>
2026-01-01 12:32:38 +0530

    Initial commit

---
+++ bin.py
@@ -0,0 +1,15 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+sorted_list = [1, 5, 10, 15, 20, 25]
+print(f"Index of 10 is: {binary_search(sorted_list, 10)}")
