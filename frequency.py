49e05ee386bd3a005909389137a432b9710b0453
GVanithasri <gvvanithasri3636@gmail.com>
2026-01-19 10:59:13 +0530

    code to run in jenkins

--- frequency.py
+++ frequency.py
@@ -1,7 +1,4 @@
-n1 = int(input("Enter the number1"))
-n2 = int(input("Enter the number2"))
-a = n1+n2
-print(a,"is the result")
+#finding frequency of string
 def char_freq(s):
     dict1={}
     for i in s:
@@ -10,6 +7,23 @@ def char_freq(s):
         else:
             dict[i]+=1
     return(dict.items())
-str1=input("enter the string")
+str1="kishorekumar23MIS0171"
 dict1=char_freq(str1)
 print(dict1)
+
+#binary search
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+sorted_list = [1, 5, 10, 15, 20, 25]
+print(f"Index of 10 is: {binary_search(sorted_list, 10)}")
